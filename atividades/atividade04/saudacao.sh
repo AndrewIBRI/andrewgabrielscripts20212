cbc70114628e315d2870534eb47da3f674eb229c
Andrew Gabriel <55504372+AndrewIBRI@users.noreply.github.com>
2021-10-28 22:36:45 -0300

    atividade 04
    solucao da atividade 04

---
+++ atividades/atividade04/saudacao.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Olá $USER,"
+echo "Olá $USER," >> saudacao.log
+echo "Hoje é dia $(date +%d), do mês $(date +%m) do ano de $(date +%G)" >> saudacao.log
+echo "Hoje é dia $(date +%d), do mês $(date +%m) do ano de $(date +%G)"
