f29d3b8241381fcbc53decc74a6127f71b836d69
Andrew Gabriel <55504372+AndrewIBRI@users.noreply.github.com>
2021-12-04 18:42:38 -0300

    atividade09

---
+++ atividades/atividade09/hosts.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+comando1=$1
+host=$2
+comando2=$3
+ip=$4
+
+while getopts "adlr" OPTVAR
+do
+	case $OPTVAR in
+		a)
+			if [ $comando2 == '-i' ] ; then
+				echo "$host	$ip" >> hosts.db
+			fi
+			;;
+		l)
+			cat hosts.db
+			;;
+		r)
+			grep $2 hosts.db | cut -f1
+			;;
+		d)
+			sed -i '/'$host'/d' hosts.db
+			;;
+		*)
+			;;
+	esac
+
+done
+case $comando1 in
+		$1)
+			grep $2 hosts.db | cut -f2
+			;;
+esac
