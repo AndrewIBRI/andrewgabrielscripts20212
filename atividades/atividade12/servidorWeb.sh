66fec2864b26c26b8a7e2da38e8d232803acad9c
joaomarceloalencar <joao.marcelo@ufc.br>
2022-01-20 10:22:06 -0300

    Correção da Atividade 12

--- atividades/atividade12/servidorWeb.sh
+++ atividades/atividade12/servidorWeb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Correção: 0,5. Ganhou a presença, mas o script não executa. Também não instala o servidor web. A maneira com que a variável "ip" é declarada está errada.
 echo "Criando servidor..."
 aws ec2 run-instances --image-id ami-042e8287309f5df03 --instance-type "t2.micro" --key-name $1
 ip= aws ec2 describe-instances --instance-ids {$(aws iam get-user --UserId)} --query 'Reservations[*].Instances[*].PublicIpAddress' --output text
