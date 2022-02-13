ade3c410137ae30fc87842dfff9951ba6a98c16f
Andrew Gabriel <55504372+AndrewIBRI@users.noreply.github.com>
2022-02-12 21:44:39 -0300

    backup

--- atividades/atividade17/wordpressRDS.sh
+++ atividades/atividade17/wordpressRDS.sh
@@ -12,18 +12,18 @@ sed -i '35s/^/PASSWORD='$3'\n/' mq2.txt
 pending=true
 subnet=$(aws ec2 describe-subnets --query "Subnets[0].SubnetId" --output text)
 imagem=ami-04505e74c0741db8d
-grupid=$(aws ec2 create-security-group --group-name andrew --description "atividade16mysql" --output text)
+grupid=$(aws ec2 create-security-group --group-name andrew --description "atividade15mysql" --output text)
 aws ec2 authorize-security-group-ingress --group-name andrew --port 22 --protocol tcp --cidr $ipdohost/0 >/dev/null 2>&1
 aws ec2 authorize-security-group-ingress --group-name andrew --port 80 --protocol tcp --cidr 0.0.0.0/0 >/dev/null 2>&1
 aws ec2 authorize-security-group-ingress --group-name andrew --port 3306 --protocol tcp --source-group $grupid >/dev/null 2>&1
-aws ec2 run-instances --image-id $imagem --instance-type "t2.micro" --key-name $1 --security-group-ids $grupid --subnet-id $subnet --user-data file://mq1.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=bancodedados}]' >/dev/null 
-aws rds create-db-instance --db-instance-identifier test-mysql-instance --db-instance-class db.t2.micro --engine mysql --master-username admin --master-user-password secret99 --allocated-storage 20 
+aws rds create-db-instance --db-instance-identifier wordpress --vpc-security-group-ids $grupid --db-instance-class db.t2.micro --engine mysql --master-username wordpress --master-user-password 123alu456 --allocated-storage 20 >/dev/null 
+
 while [ $pending == true ]
 do
 echo "Criando servidor de Banco de Dados..."
-   if [ $(aws ec2 describe-instances --filters 'Name=tag:Name,Values=bancodedados'   --output text --query 'Reservations[].Instances[].[State.Name]') == "running" ]; then
+   if [ $(aws rds describe-instances --output text --query 'Reservations[].Instances[].[State.Name]') == "running" ]; then
    pending=false
-   ip1=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=bancodedados'   --output text --query 'Reservations[].Instances[].PrivateIpAddress')
+   ip1=$(aws rds describe-instances --output text --query 'Reservations[].Instances[].PrivateIpAddress')
    fi
 done
 
